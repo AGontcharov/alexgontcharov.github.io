9468c41e660a0ca6073739672d5e952741512b71
Alexander Gontcharov <alexander.gontcharov@entrustdatacard.com>
2018-05-21 17:59:46 -0400

    Add react-boostrap and other devDependencies

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// CSS
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 import './index.css';
+
+// Components
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
