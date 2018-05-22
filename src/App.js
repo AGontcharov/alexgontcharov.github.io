380a9a38aedeacc445fa2026c339d12bbca5576d
Alexander Gontcharov <alexander.gontcharov@entrustdatacard.com>
2018-05-22 01:38:05 -0400

    Add prettier config file

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import * as React from 'react';
 
 class App extends React.Component<React.Node> {
   render() {
-    return (
-      <div>
-        Still works.
-      </div>
-    );
+    return <div>Still works</div>;
   }
 }
 
